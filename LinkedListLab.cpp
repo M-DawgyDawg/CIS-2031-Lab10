6a6f2360b3fb269ca073b6ae094a93f72279662f
Mary Titus <mdt02020@csc.vsc.edu>
2022-11-03 10:36:53 -0400

    yes

--- LinkedListLab.cpp
+++ LinkedListLab.cpp
@@ -7,11 +7,10 @@ using std::endl;
 
 struct List {
     string value;
-    List* next;
+    List* next;  
 };
 
-//This function prints out all the values in a
-//List, one on each line.
+
 void printList( List* l ){
     cout << endl << "== The List ==" << endl;
     if ( l == NULL ){
@@ -26,35 +25,52 @@ void printList( List* l ){
 
 //This function adds a new value to the
 //start of the list.
+//❓ Lab Question 3
 void prepend(List* &list, string val){
+    //❓ Lab Question 4
+    //❓ Lab Question 5
     list = new List{val, list};
 }
 
-//This function removes the first item from the list
 void removeFirst(List* &list){
     //❓ Lab Question 9
-    dinner = dinner->next;
-    delete turnip;
+    List* temp = list;
+    list = list->next;
+    delete temp; 
 }
 
+//This function returns true if the list haystck
+//contains the value needle, and false otherwise.
+bool contains(List* haystack, string needle){
+    //❓ Lab Question 10
+    //YOUR CODE HERE
+    return false;
+}
+
+void insertAt(List* &list, int pos, string value){
+    //❓ Lab Question 11
+    //YOUR CODE HERE
+}
+
+void deleteAt(List* &list, int pos, string value){
+    //❓ Lab Question 11
+    //YOUR CODE HERE
+}
 
 void main(){
     //Create an empty list and print it
     List* dinner = NULL;
     printList(dinner);
 
-    //Add food one item at a time using the
-    //prepend function, and print it
     prepend(dinner, "Pie");
     prepend(dinner, "Turkey");
     prepend(dinner, "Stuffing");
     prepend(dinner, "Mashed Potatoes");
-    prepend(dinner, "Turnips"); 
+    prepend(dinner, "Turnips"); //❓ Lab Question 7
     printList(dinner);
 
-
+    //❓ Lab Question 6
     // YOUR CODE TO REMOVE TURNIPS HERE:
-    List* turnip = dinner;
     removeFirst(dinner);
     printList(dinner);
 
